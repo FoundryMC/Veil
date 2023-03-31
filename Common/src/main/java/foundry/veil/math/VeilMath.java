fa934af883822aeaa29152822ed6a5aeb09ae0c2
amo <lunarteclipse@gmail.com>
2023-03-31 01:50:01 +0100

    oopsy doopsy

--- Common/src/main/java/foundry/veil/math/VeilMath.java
+++ Common/src/main/java/foundry/veil/math/VeilMath.java
@@ -1,6 +1,6 @@
 package foundry.veil.math;
 
-public class Math {
+public class VeilMath {
     public static double remap(double value, double from1, double to1, double from2, double to2) {
         return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
--- Common/src/main/java/foundry/veil/math/Waves.java
+++ Common/src/main/java/foundry/veil/math/Waves.java
@@ -2,6 +2,6 @@ package foundry.veil.math;
 
 public class Waves {
     public static double approxGerstner(float x){
-        return 1-Math.sqrt(2*(Math.sin(2*x)+1))/2;
+        return 1- Math.sqrt(2*(Math.sin(2*x)+1))/2;
     }
 }
