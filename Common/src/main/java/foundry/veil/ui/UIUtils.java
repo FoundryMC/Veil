f5da1c7b50511465d3aa2ceb9a2d32c96aeaf114
amo <lunarteclipse@gmail.com>
2023-03-31 20:36:03 +0100

    pls dont crash

--- Common/src/main/java/foundry/veil/ui/UIUtils.java
+++ Common/src/main/java/foundry/veil/ui/UIUtils.java
@@ -149,12 +149,6 @@ public class UIUtils {
         drawGradientRect(mat, z, tooltipX - 3, tooltipY + tooltipHeight + 2,
                 tooltipX + tooltipTextWidth + 3, tooltipY + tooltipHeight + 3, borderColorEnd, borderColorEnd);
 
-        Minecraft.getInstance().getItemRenderer().blitOffset += 300;
-        for (VeilUIItemTooltipDataHolder item : items) {
-            renderAndDecorateItem(item.getItemStack(), tooltipX + item.getX().apply(pticks), tooltipY + item.getY().apply(pticks));
-            drawTexturedRect(mat, z, tooltipX + item.getX().apply(pticks), tooltipY + item.getY().apply(pticks), 16, 16, 0, 0, 0, 0, 16, 16, Veil.veilPath("textures/gui/item_shadow"));
-        }
-        Minecraft.getInstance().getItemRenderer().blitOffset -= 300;
 
         MultiBufferSource.BufferSource renderType = MultiBufferSource.immediate(Tesselator.getInstance()
                 .getBuilder());
@@ -174,6 +168,12 @@ public class UIUtils {
             tooltipY += 10;
         }
 
+        Minecraft.getInstance().getItemRenderer().blitOffset += 300;
+        for (VeilUIItemTooltipDataHolder item : items) {
+            renderAndDecorateItem(item.getItemStack(), tooltipX + item.getX().apply(pticks), tooltipY + item.getY().apply(pticks));
+            drawTexturedRect(mat, z, tooltipX + item.getX().apply(pticks), tooltipY + item.getY().apply(pticks), 16, 16, 0, 0, 0, 0, 16, 16, Veil.veilPath("textures/gui/item_shadow.png"));
+        }
+        Minecraft.getInstance().getItemRenderer().blitOffset -= 300;
 
         renderType.endBatch();
         pStack.popPose();
