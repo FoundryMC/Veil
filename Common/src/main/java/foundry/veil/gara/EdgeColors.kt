be92534c9c558869f16400c6413f95fb930ecda2
Ocelot <FinntheRaider@gmail.com>
2023-06-30 22:43:46 -0600

    Kotlin is broken :cry:

--- Common/src/main/java/foundry/veil/gara/EdgeColors.kt
+++ Common/src/main/java/foundry/veil/gara/EdgeColors.kt
@@ -1,10 +1,10 @@
-package foundry.veil.gara
-
-import foundry.veil.color.Color
-
-/**
- * Stores 4 colors, representing the edges of a box
- */
-class EdgeColors(val top: Color, val right: Color, val bottom: Color, val left: Color) {
-    constructor(uniform: Color) : this(uniform, uniform, uniform, uniform)
-}
+//package foundry.veil.gara
+//
+//import foundry.veil.color.Color
+//
+///**
+// * Stores 4 colors, representing the edges of a box
+// */
+//class EdgeColors(val top: Color, val right: Color, val bottom: Color, val left: Color) {
+//    constructor(uniform: Color) : this(uniform, uniform, uniform, uniform)
+//}
