90f9c87faa756b814f4971039dd1306f083067e2
amo <lunarteclipse@gmail.com>
2023-07-09 21:39:15 +0100

    jdoc broked

--- Common/src/main/java/foundry/veil/shader/RenderStateShardRegistry.java
+++ Common/src/main/java/foundry/veil/shader/RenderStateShardRegistry.java
@@ -7,21 +7,21 @@ import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 
 public class RenderStateShardRegistry {
-    /**
-     * Create and initialize a {@link RenderStateShard.OutputStateShard} with the given name.
-     * When initializing your {@link RenderType}, use the method {@link RenderType.CompositeState.CompositeStateBuilder#setOutputState(RenderStateShard.OutputStateShard)} with the output state you created.
-     * <p>
-     * In order to use this, create a shader that has a target and an auxtarget with the same ID as your {@link RenderTarget}.
-     * You can now use this auxtarget as a sampler in your shader.
-     * Remember to combine the output of your post-processing shader with the main {@link RenderTarget} in another post-processing shader.
-     * You can permanently add a post-processing shader to the screen by adding it to the {@link PostProcessingHandler}.
-     * <p>
-     *     Example:
-     * </p>
-     */
-    public static final RenderStateShard.OutputStateShard VEIL_CUSTOM = new RenderStateShard.OutputStateShard("veil:custom", () -> {
-        RenderTargetRegistry.renderTargetObjects.get("veil:custom").bindWrite(false);
-    }, () -> {
-        RenderTargetRegistry.renderTargetObjects.get("veil:custom").bindWrite(false);
-    });
+//    /**
+//     * Create and initialize a {@link RenderStateShard.OutputStateShard} with the given name.
+//     * When initializing your {@link RenderType}, use the method {@link RenderType.CompositeState.CompositeStateBuilder#setOutputState(RenderStateShard.OutputStateShard)} with the output state you created.
+//     * <p>
+//     * In order to use this, create a shader that has a target and an auxtarget with the same ID as your {@link RenderTarget}.
+//     * You can now use this auxtarget as a sampler in your shader.
+//     * Remember to combine the output of your post-processing shader with the main {@link RenderTarget} in another post-processing shader.
+//     * You can permanently add a post-processing shader to the screen by adding it to the {@link PostProcessingHandler}.
+//     * <p>
+//     *     Example:
+//     * </p>
+//     */
+//    public static final RenderStateShard.OutputStateShard VEIL_CUSTOM = new RenderStateShard.OutputStateShard("veil:custom", () -> {
+//        RenderTargetRegistry.renderTargetObjects.get("veil:custom").bindWrite(false);
+//    }, () -> {
+//        RenderTargetRegistry.renderTargetObjects.get("veil:custom").bindWrite(false);
+//    });
 }
