fdc6215160230c64c31eda54eaebb7b8b579421d
amo <lunarteclipse@gmail.com>
2023-09-29 01:33:11 +0100

    rewrite filters to not be enum based - small optimization helper class

--- Common/src/main/java/foundry/veil/VeilClient.java
+++ Common/src/main/java/foundry/veil/VeilClient.java
@@ -1,6 +1,7 @@
 package foundry.veil;
 
 import foundry.veil.color.Color;
+import foundry.veil.optimization.OptimizationUtil;
 import foundry.veil.render.pipeline.VeilRenderSystem;
 import foundry.veil.render.shader.RenderTypeRegistry;
 
@@ -16,5 +17,8 @@ public class VeilClient {
 
     public static void tickClient(int ticks, float partialTick) {
         Color.tickRainbow(ticks, partialTick);
+        if(ticks % 200 == 0){
+            OptimizationUtil.calculateStableFps();
+        }
     }
 }
--- Common/src/main/java/foundry/veil/color/Filter.java
+++ Common/src/main/java/foundry/veil/color/Filter.java
@@ -5,24 +5,17 @@ public class Filter {
         return type.apply(color, value);
     }
 
-    public enum FilterType implements IFilterType {
-        sepia {
-            @Override
-            public Color apply(Color color, float value) {
-                float r = (color.getRed() * .393f) + (color.getGreen() * .769f) + (color.getBlue() * .189f);
-                float g = (color.getRed() * .349f) + (color.getGreen() * .686f) + (color.getBlue() * .168f);
-                float b = (color.getRed() * .272f) + (color.getGreen() * .534f) + (color.getBlue() * .131f);
-                return new Color(r, g, b, color.getAlpha());
-            }
-        },
-        grayscale {
-            @Override
-            public Color apply(Color color, float value) {
-                float average = (color.getRed() + color.getGreen() + color.getBlue()) / 3.0f;
-                return new Color(average, average, average, color.getAlpha());
-            }
-        }
-    }
+    public static final IFilterType SEPIA = (color, value) -> {
+        float r = (color.getRed() * .393f) + (color.getGreen() * .769f) + (color.getBlue() * .189f);
+        float g = (color.getRed() * .349f) + (color.getGreen() * .686f) + (color.getBlue() * .168f);
+        float b = (color.getRed() * .272f) + (color.getGreen() * .534f) + (color.getBlue() * .131f);
+        return new Color(r, g, b, color.getAlpha());
+    };
+
+    public static final IFilterType GRAYSCALE = (color, value) -> {
+        float average = (color.getRed() + color.getGreen() + color.getBlue()) / 3.0f;
+        return new Color(average, average, average, color.getAlpha());
+    };
     public interface IFilterType {
         Color apply(Color color, float value);
     }
