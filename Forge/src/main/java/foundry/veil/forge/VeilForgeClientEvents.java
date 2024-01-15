45535245ec1f5c99c0920ebeb604c54c64d3c5a7
Ocelot <FinntheRaider@gmail.com>
2024-01-15 09:15:58 -0700

    Fixed lights not being cleared when leaving level

--- Forge/src/main/java/foundry/veil/forge/VeilForgeClientEvents.java
+++ Forge/src/main/java/foundry/veil/forge/VeilForgeClientEvents.java
@@ -2,28 +2,17 @@ package foundry.veil.forge;
 
 import foundry.veil.Veil;
 import foundry.veil.VeilClient;
-import foundry.veil.render.deferred.VeilDeferredRenderer;
 import foundry.veil.render.pipeline.VeilRenderSystem;
 import foundry.veil.render.ui.VeilUITooltipRenderer;
 import net.minecraft.client.Minecraft;
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.PathPackResources;
-import net.minecraft.server.packs.repository.Pack;
-import net.minecraft.server.packs.repository.PackSource;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
-import net.minecraftforge.event.AddPackFindersEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.loading.FMLLoader;
 import org.jetbrains.annotations.ApiStatus;
-
-import java.nio.file.Path;
 
 import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
 
@@ -55,23 +44,7 @@ public class VeilForgeClientEvents {
     }
 
     @SubscribeEvent
-    public void addPackFinders(AddPackFindersEvent event) {
-        if (event.getPackType() == PackType.CLIENT_RESOURCES) {
-
-            // Register test resource pack
-            if (Veil.DEBUG && !FMLLoader.isProduction()) {
-                registerBuiltinPack(event, Veil.veilPath("test_shaders"));
-            }
-
-            // TODO make this pack enabled by default
-            registerBuiltinPack(event, VeilDeferredRenderer.PACK_ID);
-        }
-    }
-
-    private static void registerBuiltinPack(AddPackFindersEvent event, ResourceLocation id) {
-        Path resourcePath = ModList.get().getModFileById(Veil.MODID).getFile().findResource("resourcepacks/" + id.getPath());
-        Pack pack = Pack.readMetaAndCreate(id.toString(), Component.literal(id.getNamespace() + "/" + id.getPath()), false,
-                (path) -> new PathPackResources(path, resourcePath, false), PackType.CLIENT_RESOURCES, Pack.Position.BOTTOM, PackSource.BUILT_IN);
-        event.addRepositorySource(packConsumer -> packConsumer.accept(pack));
+    public static void leaveGame(ClientPlayerNetworkEvent.LoggingOut event) {
+        VeilRenderSystem.renderer().getDeferredRenderer().reset();
     }
 }
