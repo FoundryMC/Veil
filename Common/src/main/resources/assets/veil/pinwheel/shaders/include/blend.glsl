490da4e02fe6337031da31aa18c8d2d97482deb6
Ocelot <FinntheRaider@gmail.com>
2024-01-15 13:46:51 -0700

    Fixed blending not working

--- Common/src/main/resources/assets/veil/pinwheel/shaders/include/blend.glsl
+++ Common/src/main/resources/assets/veil/pinwheel/shaders/include/blend.glsl
@@ -1,12 +1,12 @@
-#define TRANSLUCENT_TRANSPARENCY 0.0
-#define ADDITIVE_TRANSPARENCY 1.0
-#define LIGHTNING_TRANSPARENCY 2.0
-#define GLINT_TRANSPARENCY 3.0
-#define CRUMBLING_TRANSPARENCY 4.0
-#define NO_TRANSPARENCY 5.0
+#define TRANSLUCENT_TRANSPARENCY 0
+#define ADDITIVE_TRANSPARENCY 1
+#define LIGHTNING_TRANSPARENCY 2
+#define GLINT_TRANSPARENCY 3
+#define CRUMBLING_TRANSPARENCY 4
+#define NO_TRANSPARENCY 5
 
 vec3 blend(vec4 dst, vec4 src) {
-    return src.rgb + (dst.rgb * (1.0 - src.a));
+    return src.rgb + (dst.rgb * (1 - src.a));
 }
 
 vec3 blendAdditive(vec4 dst, vec4 src) {
@@ -25,23 +25,23 @@ vec3 blendCrumbling(vec4 dst, vec4 src) {
     return src.rgb * dst.rgb + dst.rgb * src.rgb;
 }
 
-vec3 blend(float material, vec4 dst, vec4 src) {
+vec3 blend(uint material, vec4 dst, vec4 src) {
     if (material != NO_TRANSPARENCY) {
-        if (abs(material - TRANSLUCENT_TRANSPARENCY) < 0.1) {
+        if (material == TRANSLUCENT_TRANSPARENCY) {
             return blend(dst, src);
         }
-        if (abs(material - ADDITIVE_TRANSPARENCY) < 0.1) {
+        if (material == ADDITIVE_TRANSPARENCY) {
             return blendAdditive(dst, src);
         }
-        if (abs(material - LIGHTNING_TRANSPARENCY) < 0.1) {
+        if (material == LIGHTNING_TRANSPARENCY) {
             return blendLightning(dst, src);
         }
-        if (abs(material - GLINT_TRANSPARENCY) < 0.1) {
+        if (material == GLINT_TRANSPARENCY) {
             return blendGlint(dst, src);
         }
-        if (abs(material - CRUMBLING_TRANSPARENCY) < 0.1) {
+        if (material == CRUMBLING_TRANSPARENCY) {
             return blendCrumbling(dst, src);
         }
     }
-    return src.a == 0.0 ? dst.rgb : src.rgb;
+    return src.a == 0 ? dst.rgb : src.rgb;
 }
