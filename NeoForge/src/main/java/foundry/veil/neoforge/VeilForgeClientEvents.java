67a047faafe29148465ba3e54fde173ad622bdfb
Ocelot <FinntheRaider@gmail.com>
2024-02-15 22:39:40 -0700

    Updated NeoForge

--- NeoForge/src/main/java/foundry/veil/neoforge/VeilForgeClientEvents.java
+++ NeoForge/src/main/java/foundry/veil/neoforge/VeilForgeClientEvents.java
@@ -1,14 +1,26 @@
 package foundry.veil.neoforge;
 
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import foundry.veil.Veil;
 import foundry.veil.VeilClient;
 import foundry.veil.api.client.render.VeilRenderSystem;
+import foundry.veil.api.quasar.data.QuasarParticles;
+import foundry.veil.api.quasar.particle.ParticleEmitter;
+import foundry.veil.api.quasar.particle.ParticleSystemManager;
 import net.minecraft.client.Minecraft;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.ResourceLocationArgument;
+import net.minecraft.commands.arguments.coordinates.Vec3Argument;
+import net.minecraft.commands.arguments.coordinates.WorldCoordinates;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
 import net.neoforged.neoforge.client.event.InputEvent;
+import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
 import net.neoforged.neoforge.event.TickEvent;
 import org.jetbrains.annotations.ApiStatus;
 
@@ -29,6 +41,36 @@ public class VeilForgeClientEvents {
     public static void keyPressed(InputEvent.Key event) {
         if (event.getAction() == GLFW_PRESS && VeilClient.EDITOR_KEY.matches(event.getKey(), event.getScanCode())) {
             VeilRenderSystem.renderer().getEditorManager().toggle();
+        }
+    }
+
+    @SubscribeEvent
+    public static void registerClientCommands(RegisterClientCommandsEvent event) {
+        LiteralArgumentBuilder<CommandSourceStack> builder = Commands.literal("quasar");
+        builder.then(Commands.argument("emitter", ResourceLocationArgument.id()).suggests(QuasarParticles.emitterSuggestionProvider()).then(Commands.argument("position", Vec3Argument.vec3()).executes(ctx -> {
+            ResourceLocation id = ResourceLocationArgument.getId(ctx, "emitter");
+
+            CommandSourceStack source = ctx.getSource();
+            ParticleSystemManager particleManager = VeilRenderSystem.renderer().getParticleManager();
+            ParticleEmitter instance = particleManager.createEmitter(id);
+            if (instance == null) {
+                source.sendFailure(Component.literal("Unknown emitter: " + id));
+                return 0;
+            }
+
+            WorldCoordinates pos = ctx.getArgument("position", WorldCoordinates.class);
+            instance.setPosition(pos.getPosition(source));
+            particleManager.addParticleSystem(instance);
+            source.sendSuccess(() -> Component.literal("Spawned " + id), true);
+            return 1;
+        })));
+        event.getDispatcher().register(builder);
+    }
+
+    @SubscribeEvent
+    public void tick(TickEvent.LevelTickEvent event) {
+        if (event.phase == TickEvent.Phase.START && event.side.isClient()) {
+            VeilRenderSystem.renderer().getParticleManager().tick();
         }
     }
 
--- NeoForge/src/main/java/foundry/veil/neoforge/mixin/client/deferred/ParticleEngineMixin.java
+++ NeoForge/src/main/java/foundry/veil/neoforge/mixin/client/deferred/ParticleEngineMixin.java
@@ -1,30 +1,28 @@
 package foundry.veil.neoforge.mixin.client.deferred;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import foundry.veil.api.client.render.VeilRenderSystem;
 import foundry.veil.impl.client.render.deferred.DeferredShaderStateCache;
-import it.unimi.dsi.fastutil.objects.Object2ObjectArrayMap;
 import net.minecraft.client.Camera;
 import net.minecraft.client.particle.ParticleEngine;
-import net.minecraft.client.particle.ParticleRenderType;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.ShaderInstance;
 import net.minecraft.client.renderer.culling.Frustum;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.util.Iterator;
+import java.util.function.Supplier;
 
 @Mixin(ParticleEngine.class)
 public class ParticleEngineMixin {
 
     @Unique
-    private final Object2ObjectArrayMap<ParticleRenderType, DeferredShaderStateCache> veil$cache = new Object2ObjectArrayMap<>();
+    private final DeferredShaderStateCache veil$cache = new DeferredShaderStateCache();
 
     @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;Lnet/minecraft/client/renderer/LightTexture;Lnet/minecraft/client/Camera;FLnet/minecraft/client/renderer/culling/Frustum;)V", at = @At("HEAD"), remap = false)
     public void setupRenderState(PoseStack p_107337_, MultiBufferSource.BufferSource p_107338_, LightTexture p_107339_, Camera p_107340_, float p_107341_, Frustum clippingHelper, CallbackInfo ci) {
@@ -36,13 +34,8 @@ public class ParticleEngineMixin {
         VeilRenderSystem.renderer().getDeferredRenderer().clear();
     }
 
-    @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;Lnet/minecraft/client/renderer/LightTexture;Lnet/minecraft/client/Camera;FLnet/minecraft/client/renderer/culling/Frustum;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/ParticleRenderType;begin(Lcom/mojang/blaze3d/vertex/BufferBuilder;Lnet/minecraft/client/renderer/texture/TextureManager;)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILSOFT, remap = false)
-    public void setShader(PoseStack p_107337_, MultiBufferSource.BufferSource p_107338_, LightTexture p_107339_, Camera p_107340_, float p_107341_, Frustum clippingHelper, CallbackInfo ci, PoseStack posestack, Iterator<ParticleRenderType> var8, ParticleRenderType type) {
-        this.veil$cache.computeIfAbsent(type, unused -> new DeferredShaderStateCache()).setupRenderState(RenderSystem.getShader());
-    }
-
-    @Inject(method = "clearParticles", at = @At("TAIL"))
-    public void clearParticles(CallbackInfo ci) {
-        this.veil$cache.clear();
+    @ModifyArg(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;Lnet/minecraft/client/renderer/LightTexture;Lnet/minecraft/client/Camera;FLnet/minecraft/client/renderer/culling/Frustum;)V", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShader(Ljava/util/function/Supplier;)V"), index = 0, remap = false)
+    public Supplier<ShaderInstance> setShader(Supplier<ShaderInstance> value) {
+        return () -> this.veil$cache.getShader(value.get());
     }
 }
