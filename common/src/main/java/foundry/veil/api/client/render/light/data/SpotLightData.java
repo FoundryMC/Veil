71b04c668e79bb854e07c91e32f8b0b82ab48c97
Redcat_XVIII <124214835+redboi18@users.noreply.github.com>
2026-07-27 14:41:13 -0700

    Spot Light (#167)

---
+++ common/src/main/java/foundry/veil/api/client/render/light/data/SpotLightData.java
@@ -0,0 +1,312 @@
+package foundry.veil.api.client.render.light.data;
+
+import foundry.veil.api.client.color.Colorc;
+import foundry.veil.api.client.editor.EditorAttributeProvider;
+import foundry.veil.api.client.registry.LightTypeRegistry;
+import foundry.veil.api.client.render.CullFrustum;
+import foundry.veil.api.client.render.light.DDALightData;
+import foundry.veil.api.client.render.light.InstancedLightData;
+import imgui.ImGui;
+import net.minecraft.client.Camera;
+import net.minecraft.util.Mth;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.ApiStatus;
+import org.joml.*;
+
+import java.lang.Math;
+import java.nio.ByteBuffer;
+
+/**
+ * Represents a light emitting quad in the world.
+ *
+ * @since 2.0.0
+ */
+public class SpotLightData extends LightData implements InstancedLightData, DDALightData, EditorAttributeProvider {
+
+    private static final float MAX_ANGLE_SIZE = (float) (65535.0 / 2.0 / Math.PI);
+
+    protected final Vector3d position;
+    protected final Quaternionf orientation;
+    private final Matrix4d matrix;
+
+    protected float size;
+
+    protected float angle;
+    protected float distance;
+    protected boolean occlusionEnabled;
+
+    public SpotLightData() {
+        this.matrix = new Matrix4d();
+        this.position = new Vector3d();
+        this.orientation = new Quaternionf();
+
+        this.size = (float) Math.toRadians(45);
+
+        this.angle = (float) Math.toRadians(45);
+        this.distance = 1.0F;
+        this.occlusionEnabled = false;
+    }
+
+    /**
+     * @deprecated No longer used
+     */
+    @ApiStatus.ScheduledForRemoval(inVersion = "5.0.0")
+    @Deprecated
+    protected void updateMatrix() {
+        Quaternionfc orientation = this.getOrientation();
+        this.matrix.rotation(orientation).translate(this.position);
+    }
+
+    @Override
+    public LightTypeRegistry.LightType<?> getType() {
+        return LightTypeRegistry.SPOT.get();
+    }
+
+    /**
+     * @return The XYZ position of this light in the world
+     * @apiNote The return type will change to {@link Vector3dc} in 5.0.0. Use {@link #getPositionMutable()} to mutate this
+     */
+    public Vector3d getPosition() {
+        return this.position;
+    }
+
+    /**
+     * Allows the value to be safely modified.
+     *
+     * @return The XYZ position of this light in the world
+     * @since 4.3.0
+     */
+    public Vector3d getPositionMutable() {
+        this.markDirty();
+        return this.position;
+    }
+
+    /**
+     * @return The current orientation of the light
+     * @apiNote The return type will change to {@link Quaternionfc} in 5.0.0. Use {@link #getOrientationMutable()} to mutate this
+     */
+    public Quaternionf getOrientation() {
+        return this.orientation;
+    }
+
+    /**
+     * Allows the value to be safely modified.
+     *
+     * @return The current orientation of the light
+     * @since 4.3.0
+     */
+    public Quaternionf getOrientationMutable() {
+        return this.orientation;
+    }
+
+    /**
+     * @return The size of the light's surface
+     * @apiNote The return type will change to {@link Vector2fc} in 5.0.0.
+     */
+    public float getSize() {
+        return this.size;
+    }
+
+    /**
+     * @return The maximum angle of the light from the plane's surface.
+     */
+    public float getAngle() {
+        return this.angle;
+    }
+
+    /**
+     * @return The maximum distance the light can travel
+     */
+    public float getDistance() {
+        return this.distance;
+    }
+
+    @Override
+    public boolean isOcclusionEnabled() {
+        return this.occlusionEnabled;
+    }
+
+    /**
+     * Sets the size of the light's surface
+     *
+     * @param size The size, in blocks, of the light's surface.
+     */
+    public SpotLightData setSize(float size) {
+        this.size = size;
+        this.markDirty();
+        return this;
+    }
+
+    /**
+     * Sets the maximum angle the light can influence.
+     *
+     * @param angle The maximum angle of the light's influence in radians
+     */
+    public SpotLightData setAngle(float angle) {
+        this.angle = angle;
+        this.markDirty();
+        return this;
+    }
+
+    /**
+     * Sets the maximum distance the light can influence.
+     *
+     * @param distance The maximum area of influence for the light
+     */
+    public SpotLightData setDistance(float distance) {
+        this.distance = distance;
+        this.markDirty();
+        return this;
+    }
+
+    public SpotLightData setOcclusionEnabled(boolean occlusionEnabled) {
+        this.occlusionEnabled = occlusionEnabled;
+        this.markDirty();
+        return this;
+    }
+
+    @Override
+    public SpotLightData setColor(Vector3fc color) {
+        super.setColor(color);
+        return this;
+    }
+
+    @Override
+    public SpotLightData setColor(Colorc color) {
+        this.setColor(color.red(), color.green(), color.blue());
+        return this;
+    }
+
+    @Override
+    public SpotLightData setColor(float red, float green, float blue) {
+        super.setColor(red, green, blue);
+        return this;
+    }
+
+    @Override
+    public SpotLightData setColor(int color) {
+        super.setColor(color);
+        return this;
+    }
+
+    @Override
+    public SpotLightData setBrightness(float brightness) {
+        super.setBrightness(brightness);
+        return this;
+    }
+
+    @Override
+    public void store(ByteBuffer buffer) {
+        this.matrix.identity().rotation(this.orientation).translate(this.position).getFloats(buffer.position(), buffer);
+        buffer.position(buffer.position() + Float.BYTES * 16);
+
+        buffer.putFloat(this.color.red() * this.brightness);
+        buffer.putFloat(this.color.green() * this.brightness);
+        buffer.putFloat(this.color.blue() * this.brightness);
+
+        buffer.putFloat(this.size);
+
+        buffer.putShort((short) Mth.clamp((int) (this.angle * MAX_ANGLE_SIZE), 0, 65535));
+        buffer.putFloat(this.distance);
+        buffer.putFloat(this.occlusionEnabled ? 1.0F : 0.0F);
+    }
+
+    @Override
+    public boolean isVisible(CullFrustum frustum) {
+        float radius = this.distance;
+        return frustum.testAab(
+                this.position.x - radius,
+                this.position.y - radius,
+                this.position.z - radius,
+                this.position.x + radius,
+                this.position.y + radius,
+                this.position.z + radius);
+    }
+
+    @Override
+    public LightData setTo(Camera camera) {
+        Vec3 pos = camera.getPosition();
+        this.position.set(pos.x, pos.y, pos.z);
+        this.orientation.identity().lookAlong(camera.getLookVector().mul(-1), camera.getUpVector());
+        this.markDirty();
+        return this;
+    }
+
+    @Override
+    public void renderImGuiAttributes() {
+        Vector3f orientationAngles = this.orientation.normalize().getEulerAnglesXYZ(new Vector3f());
+
+        float[] editSize = new float[]{this.size};
+
+        double[] editX = new double[]{this.position.x()};
+        double[] editY = new double[]{this.position.y()};
+        double[] editZ = new double[]{this.position.z()};
+
+        float[] editXRot = new float[]{orientationAngles.x()};
+        float[] editYRot = new float[]{orientationAngles.y()};
+        float[] editZRot = new float[]{orientationAngles.z()};
+
+        float[] editAngle = new float[]{this.angle};
+        float[] editDistance = new float[]{this.distance};
+
+        if (ImGui.sliderAngle("size", editSize, 0.1F, 180.0F, "%.1f")) {
+            this.setSize(editSize[0]);
+        }
+
+        float totalWidth = ImGui.calcItemWidth();
+        ImGui.pushItemWidth(totalWidth / 3.0F - (ImGui.getStyle().getItemInnerSpacingX() * 0.58F));
+        if (ImGui.dragScalar("##x", editX, 0.02F)) {
+            this.position.x = editX[0];
+            this.markDirty();
+        }
+        ImGui.sameLine(0, ImGui.getStyle().getItemInnerSpacingX());
+        if (ImGui.dragScalar("##y", editY, 0.02F)) {
+            this.position.y = editY[0];
+            this.markDirty();
+        }
+        ImGui.sameLine(0, ImGui.getStyle().getItemInnerSpacingX());
+        if (ImGui.dragScalar("##z", editZ, 0.02F)) {
+            this.position.z = editZ[0];
+            this.markDirty();
+        }
+
+        ImGui.popItemWidth();
+        ImGui.sameLine(0, ImGui.getStyle().getItemInnerSpacingX());
+        ImGui.text("position");
+
+        ImGui.pushItemWidth(totalWidth / 3.0F - (ImGui.getStyle().getItemInnerSpacingX() * 0.58F));
+        if (ImGui.sliderAngle("##xrot", editXRot)) {
+            this.orientation.identity().rotationXYZ(editXRot[0], orientationAngles.y(), orientationAngles.z());
+            this.markDirty();
+        }
+        ImGui.sameLine(0, ImGui.getStyle().getItemInnerSpacingX());
+        if (ImGui.sliderAngle("##yrot", editYRot)) {
+            this.orientation.identity().rotationXYZ(orientationAngles.x(), editYRot[0], orientationAngles.z());
+            this.markDirty();
+        }
+        ImGui.sameLine(0, ImGui.getStyle().getItemInnerSpacingX());
+        if (ImGui.sliderAngle("##zrot", editZRot)) {
+            this.orientation.identity().rotationXYZ(orientationAngles.x(), orientationAngles.y(), editZRot[0]);
+            this.markDirty();
+        }
+
+        ImGui.popItemWidth();
+        ImGui.sameLine(0, ImGui.getStyle().getItemInnerSpacingX());
+        ImGui.text("orientation");
+
+        if (ImGui.sliderAngle("##angle", editAngle, 0.1F, 180.0F, "%.1f")) {
+            this.setAngle(editAngle[0]);
+        }
+        ImGui.sameLine(0, ImGui.getStyle().getItemInnerSpacingX());
+        ImGui.text("angle");
+
+        if (ImGui.dragScalar("distance", editDistance, 0.02F, 0.0F)) {
+            this.setDistance(editDistance[0]);
+        }
+
+        if (ImGui.checkbox("Occluded", this.occlusionEnabled)) {
+            this.occlusionEnabled = !this.occlusionEnabled;
+            this.markDirty();
+        }
+    }
+}
