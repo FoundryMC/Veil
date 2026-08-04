9ce460e53b9008d0bff592f88ba269fa8f8af20b
Neddslayer <neddslayer32@gmail.com>
2026-08-04 10:24:16 -0400

    Add rotation to shaking and add wiki

--- common/src/main/java/foundry/veil/mixin/screenshake/client/ScreenShakeGameRendererMixin.java
+++ common/src/main/java/foundry/veil/mixin/screenshake/client/ScreenShakeGameRendererMixin.java
@@ -5,6 +5,8 @@ import foundry.veil.api.client.render.VeilRenderSystem;
 import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.world.phys.Vec3;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,6 +19,12 @@ public class ScreenShakeGameRendererMixin {
     @Redirect(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;getPosition()Lnet/minecraft/world/phys/Vec3;"))
     private Vec3 veil$moveCameraPosition(Camera instance, @Local(name = "f") float partialTick) {
         return instance.getPosition().add(new Vec3(VeilRenderSystem.renderer().getScreenShakeManager().getPosition(partialTick)));
+    }
+
+    @Redirect(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;rotation()Lorg/joml/Quaternionf;"))
+    private Quaternionf veil$offsetCameraRotation(Camera instance, @Local(name = "f") float partialTick) {
+        Vector3f offset = VeilRenderSystem.renderer().getScreenShakeManager().getPosition(partialTick).mul(0.1f, new Vector3f());
+        return instance.rotation().rotateXYZ(offset.x, offset.y, offset.z, new Quaternionf());
     }
 
     @Inject(method = "tick", at = @At("HEAD"))
